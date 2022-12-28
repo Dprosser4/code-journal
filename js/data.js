c04775d496575ce22b6e46906f878f27b5033ef7
Dylan Prosser <dprosser4@gmail.com>
2022-12-28 00:36:02 +0000

    completed the feature and tested functionality

--- js/data.js
+++ js/data.js
@@ -11,5 +11,9 @@ window.addEventListener('beforeunload', saveData);
 function saveData(event) {
   var entriesData = JSON.stringify(data);
   localStorage.setItem('entryData', entriesData);
+}
 
+var previousData = localStorage.getItem('entryData');
+if (previousData !== null) {
+  data = JSON.parse(previousData);
 }
