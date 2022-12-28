46d0870089bf91ee1e08ae843cc1dbf58a8cc13e
Dylan Prosser <dprosser4@gmail.com>
2022-12-28 04:45:40 +0000

    finished styling and dynamic loading of elements

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 var $photourl = document.querySelector('#photourl');
-var $photoPreview = document.querySelector('.mainimage');
+var $photoPreview = document.querySelector('.main-image');
 
 function changePhoto(event) {
   var newPhotoUrl = event.target.value;
@@ -22,4 +22,36 @@ function getInputs(event) {
   data.entries.push(entryData);
   $photoPreview.setAttribute('src', 'images/placeholder-image-square.jpg');
   $entryForm.reset();
+}
+
+function renderEntry(entry) {
+  var li = document.createElement('li');
+  var imgDiv = document.createElement('div');
+  var entryImg = document.createElement('img');
+  var textDiv = document.createElement('div');
+  var titleH2 = document.createElement('h2');
+  var noteP = document.createElement('p');
+
+  imgDiv.setAttribute('class', 'column-half');
+  textDiv.setAttribute('class', 'column-half');
+  entryImg.setAttribute('class', 'main-image');
+
+  entryImg.setAttribute('src', entry.photourl);
+  titleH2.textContent = entry.title;
+  noteP.textContent = entry.note;
+
+  imgDiv.appendChild(entryImg);
+  textDiv.appendChild(titleH2);
+  textDiv.appendChild(noteP);
+  li.appendChild(imgDiv);
+  li.appendChild(textDiv);
+
+  return li;
+}
+
+var $entryList = document.querySelector('#entry-list');
+
+for (var i = 0; i < data.entries.length; i++) {
+  var storedEntry = renderEntry(data.entries[i]);
+  $entryList.appendChild(storedEntry);
 }
